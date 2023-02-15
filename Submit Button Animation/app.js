78c96a4a6b5563d7159965f6e0173ec99a1a8a52
Abbas Shaikh <61411096+Abbas12321@users.noreply.github.com>
2023-02-15 15:17:31 +0530

    Add files via upload

---
+++ Submit Button Animation/app.js
@@ -0,0 +1,13 @@
+document.querySelectorAll(".button").forEach((button) => {
+    button.addEventListener("click", (e) => {
+      button.classList.add("processing");
+      e.preventDefault();
+    });
+  });
+
+  
+
+
+
+
+//   Abbas Shaikh
